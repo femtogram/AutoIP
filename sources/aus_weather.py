c0c752b4ed7cf19681b00e2c1b73d6af1e578810
David Hedin <lucky.hedin13@gmail.com>
2013-03-26 23:46:26 -0400

    Fix Sydney Weather

--- sources/aus_weather.py
+++ sources/aus_weather.py
@@ -9,21 +9,13 @@ import re
 import HTMLParser
 
 
-def _parse_title(title_text):
-    title = re.sub("</?p>", "", title_text)
-    return str(title)
-
-
 def _parse_desc(desc_text):
-    desc_text = re.sub("<img.+/>", "", desc_text)
-    desc_text = re.sub("</?b>", "", desc_text)
-    desc_text = re.sub("<br />", "", desc_text)
-    desc_text = re.sub("\s{2,}", "\n", desc_text)
+    desc_text = re.findall("(?<=Temperature:</b> )[\d\.&#;CF]{3,20}(?=\n)", desc_text)[0]
     desc_text = HTMLParser.HTMLParser().unescape(desc_text)
     return str(desc_text)
 
 
-def get_aus_weather():
+def get_aus_weather(ip_addr):
     fetch = urllib2.urlopen("http://rss.weatherzone.com.au/?u=12994-1285&lt=aploc&lc=624&obs=1&fc=1&warn=1")
     result = fetch.read()
     try:
@@ -38,9 +30,9 @@ def get_aus_weather():
             for title in titles:
                 if title.text.startswith("Current"):
                     desc = item.findtext("description")
-                    title = _parse_title(title.text)
-                    desc = _parse_desc(desc)
-                    return "".join([title, " is:", desc, "And today's ip is: "])
+                    temp = _parse_desc(desc)
+                    return "".join(["Right now, in Sydney, Australia, the temperature is ", temp,
+                                    ", and today's IP is: ", ip_addr])
 
 if __name__ == "__main__":
-    print get_aus_weather()
+    print get_aus_weather("129.161.209.87")
